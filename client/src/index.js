781ef4ef9a79010b9114a77e3e0b1ee4da0f53db
Furkan YÜCEL <81173127+furkycl@users.noreply.github.com>
2025-09-01 07:03:17 +0300

    refactor: Clean up default create-react-app boilerplate

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+// Hatalı 'client-dom/client' yerine doğru olan 'react-dom/client' kullanılıyor
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// 'root' elementini seçiyoruz
+const rootElement = document.getElementById("root");
+// Yeni API'yi kullanarak root'u oluşturuyoruz
+const root = ReactDOM.createRoot(rootElement);
+
+// Uygulamayı render ediyoruz
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
